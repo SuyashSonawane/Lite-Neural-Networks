76ec51efa939aac24e924eeb37c076d466cc9fd4
Suyash Sonawane <suyashsonawane007@gmail.com>
2020-08-24 10:52:05 +0530

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -11,7 +11,7 @@ function CopyToClipboard(containerid) {
     document.execCommand("copy");
     swal({
       title: "Copied",
-      text: `<script src="https://firebasestorage.googleapis.com/v0/b/website-d2f19.appspot.com/o/nn-v0.js?alt=media&token=cd7b6c3b-0796-411c-a49d-89f22277119a"></script>`,
+      text: `<script src="https://cdn.jsdelivr.net/gh/suyashsonawane/Lite-Neural-Networks@latest/lib/nn-v0.js"></script>`,
       icon: "success",
     });
   }
